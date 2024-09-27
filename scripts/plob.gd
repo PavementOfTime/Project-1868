3ab75516ecf721d6c6ef28ebf3cd0f51dbb37631
Luke <lrtidball@gmail.com>
2024-09-26 19:44:58 -0500

    Added a unique sprite for 'Plob'
    and animations for him

--- scripts/plob.gd
+++ scripts/plob.gd
@@ -1,25 +1,29 @@
 extends Node2D
 @onready var attack_box: CollisionShape2D = $AttackBox/AttackBox
+@onready var wa: AnimatedSprite2D = $AnimatedSprite2D
 
 
-
-
-const SPEED = 60
+const SPEED = 40
 var canMove = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	wa.play("walk")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void: 
 	if canMove == true:
 		position.x += SPEED * delta
+	
 
 # Called when a node on layers 1 or 2 enter
 func _on_body_entered(body: Node2D) -> void:
 	canMove = false
+	wa.play("idle")
+	print("a")
+	
 
 # Called when a node on layers 1 or 2 exit
 func _on_body_exited(body: Node2D) -> void:
 	canMove = true
+	wa.play("walk")
